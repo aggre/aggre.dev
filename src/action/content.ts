e2148d0b06a112f0356097e8520f801992fd6e6e
aggre <hiroyuki.aggre@gmail.com>
2018-10-28 15:35:57 +0900

    Change code style

--- src/action/content.ts
+++ src/action/content.ts
@@ -1,9 +1,14 @@
 import { content } from '../store/content'
+import { BehaviorSubject } from 'rxjs'
 const { location } = window
 
 const convertUrl = (path: string) => `${path === '/' ? 'index' : path}.md`
 
-export const fetchContent = async (path?: string) =>
-	fetch(convertUrl(path || location.pathname))
-		.then(async res => res.text())
-		.then(text => content.next(text))
+export const fetchContent = async (
+	path?: string,
+	store: BehaviorSubject<string> = content
+) => {
+	const res = await fetch(convertUrl(path || location.pathname))
+	const text = await res.text()
+	return store.next(text)
+}
--- src/action/route.ts
+++ src/action/route.ts
@@ -1,8 +1,13 @@
 import { route } from '../store/route'
+import { BehaviorSubject } from 'rxjs'
 
 const { history } = window
 
-export const routeChange = (next: string) => [
-	history.pushState(null, '', next),
-	route.next(next)
-]
+export const routeChange = (
+	next: string,
+	store: BehaviorSubject<string> = route
+) => {
+	// tslint:disable-next-line:no-expression-statement
+	history.pushState(null, '', next)
+	return store.next(next)
+}
