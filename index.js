25e2412903412ccffc9d538e636d39b66078cde2
aggre <hiroyuki.aggre@gmail.com>
2018-11-03 21:30:12 +0900

    Implement SSR with puppeteer

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const { parse } = require('url')
-const { readFile } = require('fs')
-const { promisify } = require('util')
+const micro = require('micro')
 const handler = require('serve-handler')
+const puppeteer = require('puppeteer')
 const options = require('./serve.json')
+const port = 5000
+const format = h => (typeof h === 'string' ? h.replace(/<\!---->/g, '') : h)
+
+micro((req, res) => handler(req, res, options)).listen(port)
 
 module.exports = async (req, res) => {
 	const { pathname } = parse(req.url)
-	if (!/\..+$/.test(pathname)) {
-		return promisify(readFile)(`${__dirname}/dist/index.html`, 'utf-8')
+
+	if (/\..+$/.test(pathname)) {
+		return handler(req, res, options)
 	}
-	handler(req, res, options)
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox']
+	})
+	const page = await browser.newPage()
+	await Promise.all([
+		page.goto(`http://localhost:${port}${pathname}`, {
+			waitUntil: 'networkidle0'
+		}),
+		page.waitForSelector('x-app > *')
+	])
+	const html = await page.content()
+	await browser.close()
+	return format(html)
 }
