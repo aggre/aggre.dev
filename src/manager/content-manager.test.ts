64945296ea210e82c791b8875014afa541051f41
aggre <hiroyuki.aggre@gmail.com>
2018-11-04 23:14:05 +0900

    Add test cases

---
+++ src/manager/content-manager.test.ts
@@ -0,0 +1,10 @@
+// tslint:disable:no-expression-statement
+describe('content manager', () => {
+	it('Subscribe to content and rerender head element')
+
+	it('Subscribe to route and change history')
+
+	it('Subscribe to route and change active nav')
+
+	it('Subscribe to route and change content')
+})
