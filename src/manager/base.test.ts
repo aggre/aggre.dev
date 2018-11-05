ffacae414a565bec94e01148328ecf6ef398091c
aggre <hiroyuki.aggre@gmail.com>
2018-11-05 18:05:47 +0900

    Rename testing file

--- src/manager/base.test.ts
+++ src/manager/base.test.ts
@@ -1,5 +1,5 @@
 // tslint:disable:no-expression-statement
-describe('content manager', () => {
+describe('base manager', () => {
 	it('Subscribe to content and rerender head element')
 
 	it('Subscribe to route and change history')
