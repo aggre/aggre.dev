41ffd1f7603d5accb6bf0cefca75421aa17060b7
aggre <hiroyuki.aggre@gmail.com>
2018-11-08 00:01:28 +0900

    Fixes bug that pre-renderer

--- pre-render.js
+++ pre-render.js
@@ -6,24 +6,26 @@ const { listFiles } = require('list-files-in-dir')
 const port = 5000
 const format = h => (typeof h === 'string' ? h.replace(/<\!---->/g, '') : h)
 const write = async (path, content) => writeFile.promise(path, content)
-const getHTML = page => async pathname =>
-	Promise.all([
-		page.goto(`http://localhost:${port}${pathname}`, {
-			waitUntil: 'networkidle0'
-		}),
-		page.waitForSelector('x-app > *')
-	])
-		.then(() => page.content())
-		.then(html => html)
+const getHTML = browser => async pathname =>
+	browser
+		.newPage()
+		.then(async page =>
+			Promise.all([
+				Promise.resolve(page),
+				page.goto(`http://localhost:${port}${pathname}`, {
+					waitUntil: 'networkidle0'
+				}),
+				page.waitForSelector('x-app > *')
+			])
+		)
+		.then(async ([page]) => {
+			const html = await page.content()
+			await page.close()
+			return html
+		})
 ;(async () => {
 	const serve = await micro((req, res) =>
-		handler(req, res, {
-			public: 'dist',
-			rewrites: [
-				{ source: 'post/**', destination: '/index.html' },
-				{ source: 'page/**', destination: '/index.html' }
-			]
-		})
+		handler(req, res, { public: 'dist' })
 	).listen(port)
 	const pages = await listFiles('content').then(fls =>
 		fls
@@ -34,10 +36,8 @@ const getHTML = page => async pathname =>
 		headless: true,
 		args: ['--no-sandbox', '--disable-setuid-sandbox']
 	})
-	const page = await browser.newPage()
-	const ssr = getHTML(page)
+	const ssr = getHTML(browser)
 	const htmls = await Promise.all(pages.map(page => ssr(page)))
-	await page.close()
 	await browser.close()
 	await Promise.all(
 		pages.map((page, i) => write(`dist${page}.html`, format(htmls[i])))
