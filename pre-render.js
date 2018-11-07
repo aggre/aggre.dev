5b1a951c1d527126a6a2555538b987b25f4d3908
aggre <hiroyuki.aggre@gmail.com>
2018-11-07 21:10:27 +0900

    Pre-render

--- pre-render.js
+++ pre-render.js
@@ -3,7 +3,6 @@ const micro = require('micro')
 const handler = require('serve-handler')
 const puppeteer = require('puppeteer')
 const {listFiles} = require('list-files-in-dir')
-const options = require('./serve.json')
 const port = 5000
 const format = h => (typeof h === 'string' ? h.replace(/<\!---->/g, '') : h)
 const write = async (path, content) => writeFile.promise(path, content)
@@ -14,9 +13,14 @@ const getHTML = page => async pathname => Promise.all([
 	page.waitForSelector('x-app > *')
 ]).then(() => page.content()).then(html => html)
 
-
 ;(async () => {
-	const serve = await micro((req, res) => handler(req, res, options)).listen(port)
+	const serve = await micro((req, res) => handler(req, res, {
+		public: 'dist',
+		rewrites: [
+			{ source: 'post/**', destination: '/index.html' },
+			{ source: 'page/**', destination: '/index.html' }
+		]
+	})).listen(port)
 	const pages = await listFiles('content').then(fls => fls.filter(f => f.endsWith('.md')).map(f => f.replace(`${__dirname}/content`, '').replace('.md', '')))
 	const browser = await puppeteer.launch({
 		headless: true,
@@ -27,7 +31,7 @@ const getHTML = page => async pathname => Promise.all([
 	const htmls = await Promise.all(pages.map(page => ssr(page)))
 	await page.close()
 	await browser.close()
-	await Promise.all(pages.map((page, i) => write(`.rendered${page}.html`, format(htmls[i]))))
+	await Promise.all(pages.map((page, i) => write(`dist${page}.html`, format(htmls[i]))))
 	serve.close()
 	console.log('completed!!!')
 })()
