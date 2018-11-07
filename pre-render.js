db37f151c008f91fac0599a104037e53d2cffd4f
aggre <hiroyuki.aggre@gmail.com>
2018-11-08 00:55:59 +0900

    await

--- pre-render.js
+++ pre-render.js
@@ -6,23 +6,18 @@ const { listFiles } = require('list-files-in-dir')
 const port = 5000
 const format = h => (typeof h === 'string' ? h.replace(/<\!---->/g, '') : h)
 const write = async (path, content) => writeFile.promise(path, content)
-const getHTML = browser => async pathname =>
-	browser
-		.newPage()
-		.then(async page =>
-			Promise.all([
-				Promise.resolve(page),
-				page.goto(`http://localhost:${port}${pathname}`, {
-					waitUntil: 'networkidle0'
-				}),
-				page.waitForSelector('x-app > *')
-			]).catch(err => console.log(err, pathname))
-		)
-		.then(async ([page]) => {
-			const html = await page.content()
-			await page.close()
-			return html
-		})
+const getHTML = browser => async pathname => {
+	console.info('render start', pathname)
+	const page = await browser.newPage()
+	await page.goto(`http://localhost:${port}${pathname}`, {
+		waitUntil: 'networkidle0'
+	})
+	await page.waitForSelector('x-app > *')
+	const html = await page.content()
+	await page.close()
+	console.info('render end', pathname)
+	return html
+}
 ;(async () => {
 	const serve = await micro((req, res) =>
 		handler(req, res, { public: 'dist' })
