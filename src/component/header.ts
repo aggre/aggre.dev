37d170ca86bf1b7edfd4d511f4aabac3c2007553
aggre <hiroyuki.aggre@gmail.com>
2018-11-27 19:39:20 +0900

    run lint

--- src/component/header.ts
+++ src/component/header.ts
@@ -30,6 +30,16 @@ export const header = () =>
 		}
 		<header>
 			<div class="brand">${a({ href: '/', content: 'aggre.io' })}</div>
-			<div class="nav">${subscribe(navs, x => html`${nav(x)}`)}</div>
+			<div class="nav">
+				${
+					subscribe(
+						navs,
+						x =>
+							html`
+								${nav(x)}
+							`
+					)
+				}
+			</div>
 		</header>
 	`)
