db3099b8bdb14d48824fcdec6bfbb263b0b3e7f3
aggre <hiroyuki.aggre@gmail.com>
2018-11-27 18:51:15 +0900

    use lit-style

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -1,34 +1,12 @@
-// tslint:disable:readonly-array
-import postcss from 'postcss'
+import { createStyle } from 'lit-style'
+import { html } from 'lit-html'
 import postcssPresetEnv from 'postcss-preset-env'
-import { html, TemplateResult } from 'lit-html'
 
-type StyleValues = string[]
-
-const cache = new WeakMap<[TemplateStringsArray, StyleValues], TemplateResult>()
-const plugins: any[] = [postcssPresetEnv({ stage: 0 })]
-const transform = async (css: string) =>
-	postcss(plugins).process(css, { from: `${Math.random()}` })
-const union = (strings: TemplateStringsArray, values: StyleValues) =>
-	strings.reduce(
-		(result, current, i) =>
-			`${result}${current}${values[i] ? `${values[i]}` : ''}`,
-		''
-	)
-const styleElement = (css: string) => html`<style>${css}</style>`
-
-export const style = async (
-	strings: TemplateStringsArray,
-	...values: StyleValues
-) => {
-	const key: [TemplateStringsArray, StyleValues] = [strings, values]
-	return cache.has(key)
-		? cache.get(key)
-		: (processed => {
-				const { css } = processed
-				const styleTemplate = styleElement(css)
-				// tslint:disable-next-line:no-expression-statement
-				cache.set(key, styleTemplate)
-				return styleTemplate
-		  })(await transform(union(strings, values)))
-}
+export const style = createStyle({
+	plugins: [postcssPresetEnv({ stage: 0 })],
+	build: css => html`
+		<style>
+			${css}
+		</style>
+	`
+})
