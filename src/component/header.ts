435f5d9911960b05543914dfa363d763d75783f7
aggre <hiroyuki.aggre@gmail.com>
2018-11-27 19:13:24 +0900

    Fix incorrect return value

--- src/component/header.ts
+++ src/component/header.ts
@@ -30,6 +30,6 @@ export const header = () =>
 		}
 		<header>
 			<div class="brand">${a({ href: '/', content: 'aggre.io' })}</div>
-			<div class="nav">${subscribe(navs, x => nav(x))}</div>
+			<div class="nav">${subscribe(navs, x => html`${nav(x)}`)}</div>
 		</header>
 	`)
