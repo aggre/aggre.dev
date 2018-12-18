a9b03489bddd24741c921a7ab3e3f75c6e8afadd
aggre <hiroyuki.aggre@gmail.com>
2018-12-18 11:07:10 +0900

    Supports lit-html@1.0.0-rc.1

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -1,14 +1,20 @@
-import { createStyle } from 'lit-style'
+import { process, directive } from 'lit-style'
 import { html } from 'lit-html'
+import { until } from 'lit-html/directives/until'
 import postcssPresetEnv from 'postcss-preset-env'
 import { AcceptedPlugin } from 'postcss'
 
-export const style = createStyle({
+export const processor = process({
 	// tslint:disable-next-line:readonly-array
-	plugins: [postcssPresetEnv({ stage: 0 })] as AcceptedPlugin[],
-	build: css => html`
-		<style>
-			${css}
-		</style>
-	`
+	plugins: [postcssPresetEnv({ stage: 0 })] as AcceptedPlugin[]
 })
+
+export const style = directive(
+	processor,
+	css =>
+		html`
+			<style>
+				${until(css)}
+			</style>
+		`
+)
